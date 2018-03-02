3243d0d4c0c25ce750cede223a230e658eec5525
Uzair Minhas <uziminhas@gmail.com>
2018-03-02 02:30:48 -0500

    Wrote function for retrieving adopters

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,19 +1,34 @@
 pragma solidity ^0.4.17;
 
 contract Adoption {
-	
 
-}
+	address[16] public adopters; 
+	//an array of Ethereum addresses
 
-address[16] public adopters; //an array of Ethereum addresses
+	/**
+	Solidity requires defining the return type of a function in ()
+	**/
 
-// Adopting a pet
-function adopt(uint petId) public returns (uint) { 
-	require(petId >= 0 && petId <= 15); //ensure the pet ID is within range
+	// Adopting a pet
+	function adopt(uint petId) public returns (uint) { 
 
-	adopters[petId] = msg.sender; //the address of the person or smart contract who called this function
+	require(petId >= 0 && petId <= 15); 
+	//ensure the pet ID is within range
+
+	adopters[petId] = msg.sender; 
+	//the address of the person or smart contract who called this function
 
 	return petId;
 
+	}
+
+	//Retrieving the adopters
+	function getAdopters() public view returns (address[16]){
+		return adopters;
+	}
+
 }
+
+
+
 
